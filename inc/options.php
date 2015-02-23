e64315831a09f757923e69f1d6e798c73eb9f509
Chris Reynolds <chris@jazzsequence.com>
2015-02-23 11:06:46 -0700

    add options to options page
    #3

--- inc/options.php
+++ inc/options.php
@@ -104,6 +104,54 @@ if ( ! class_exists( 'Art_Store_Options' ) ) {
 		 */
 		function add_options_page_metabox() {
 
+			$cmb = new_cmb2_box( array(
+				'id'      => $this->metabox_id,
+				'hookup'  => false,
+				'show_on' => array(
+					'key'   => 'options-page',
+					'value' => array( $this->key )
+				)
+			) );
+
+			// create the cmb2 fields
+
+			$cmb->add_field( array(
+				'name'    => __( 'Gallery Home', 'art-store' ),
+				'desc'    => __( 'The page you are displaying your artwork on.', 'art-store' ),
+				'id'      => 'gallery_page',
+				'type'    => 'select',
+				'options' => array(), // TODO array for pages
+				'default' => 'none'
+			) );
+
+			$cmb->add_field( array(
+				'name'    => __( 'Button Image', 'art-store' ),
+				'desc'    => __( 'Set a unique button image instead of the default PayPal (or other third-party payment processor) buy now button.', 'art-store' ),
+				'id'      => 'button_image',
+				'type'    => 'file'
+			) );
+
+			$cmb->add_field( array(
+				'name'    => __( 'Enquire for price page', 'art-store' ),
+				'desc'    => __( 'If a price is left blank, a page may be set for a contact form for visitors to enquire for a price of an item.', 'art-store' ),
+				'id'      => 'enquire_for_price',
+				'type'    => 'select',
+				'options' => array(), // TODO array for pages
+				'default' => 'none'
+			) );
+
+			$cmb->add_field( array(
+				'name'    => __( 'HTML Code or URL', 'art-store' ),
+				'desc'    => __( 'Choose whether to use HTML button codes copied from PayPal (or other third-party payment processor) or a direct URL to the payment page.', 'art-store' ),
+				'id'      => 'code_or_url',
+				'type'    => 'select',
+				'options' => array(
+					'code' => __( 'HTML Code', 'art-store' ),
+					'url'  => __( 'URL', 'art-store' )
+				),
+				'default' => 'code'
+			) );
+
 		}
 
 		/**
