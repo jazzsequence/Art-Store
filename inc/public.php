27f2cbb11c3527e3c6466e91dad097ed9d8bc15e
Chris Reynolds <chris@jazzsequence.com>
2015-03-16 10:28:59 -0600

    use the currency symbol or default to $ if no currency symbol is defined

--- inc/public.php
+++ inc/public.php
@@ -62,9 +62,10 @@ if ( ! class_exists( 'Art_Store_Public' ) ) {
 						if ( 'content' == art_store_get_option( 'product_info' ) ) {
 
 							// price
-							if ( '' !== $product_information['price'] ) { ?>
+							if ( '' !== $product_information['price'] ) {
+								$currency_symbol = ( art_store_get_option( 'currency_symbol' ) ) ? art_store_get_option( 'currency_symbol' ) : '$'; ?>
 								<dt><?php _e( 'Price', 'art-store' ); ?></dt>
-								<dd><?php echo esc_attr( $product_information['price'] );?></dd>
+								<dd><?php echo $currency_symbol . esc_attr( $product_information['price'] );?></dd>
 							<?php }
 							// status, check if it's set to "enquire for price" and if a URL has been set for the enquire for price link
 							if ( 'enquire' == $product_information['status'] && 'none' !== art_store_get_option( 'enquire_for_price' ) ) { ?>
