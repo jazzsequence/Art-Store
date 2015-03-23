42f170974721ccb5b47a1183cab8167b4e9d5fb9
Chris Reynolds <chris@jazzsequence.com>
2015-03-23 10:55:00 -0600

    include the widgets file

--- art-store.php
+++ art-store.php
@@ -87,6 +87,7 @@ if ( ! class_exists( 'Art_Store' ) ) {
 				require_once( $this->directory_path . '/inc/options.php' );
 				require_once( $this->directory_path . '/inc/cmb.php' );
 				require_once( $this->directory_path . '/inc/public.php' );
+				require_once( $this->directory_path . '/inc/widgets.php' );
 			}
 
 		} /* includes() */
